361592b42a6434e3c0acdd5e39452c453a999ed6
PhilippeR26 <philippe.rostan@yahoo.fr>
2024-10-29 15:42:10 +0100

    ci: lib bump

--- src/app/components/client/LowerBanner.tsx
+++ src/app/components/client/LowerBanner.tsx
@@ -22,10 +22,10 @@ export default function LowerBanner() {
             textColor="black"
           >
             Powered by
-            <Link color="blue.700" href='https://starknetjs.com' isExternal> Starknet.js v6.11.0<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>
-             , <Link color="blue.700" href='https://github.com/PhilippeR26/starknetMerkleTree' isExternal> Starknet-merkle-tree v1.0.2<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>,
+            <Link color="blue.700" href='https://starknetjs.com' isExternal> Starknet.js v6.17.0<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>
+             , <Link color="blue.700" href='https://github.com/PhilippeR26/starknetMerkleTree' isExternal> Starknet-merkle-tree v1.0.4<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>,
             
-            <Link color="blue.700" href='https://github.com/starknet-io/get-starknet' isExternal> get-starknet v4.0.0<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>
+            <Link color="blue.700" href='https://github.com/starknet-io/get-starknet' isExternal> get-starknet v4.0.1<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>
             ,
             <Link color="blue.700" href='https://github.com/apibara/starknet-react/tree/next-v3' isExternal> starknet-react v3.0.0<ExternalLinkIcon mx='2px'></ExternalLinkIcon></Link>
             .
